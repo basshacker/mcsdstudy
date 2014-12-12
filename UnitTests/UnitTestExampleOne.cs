82a190109f0869ee0a9a9a9d72a51c257a857562
basshacker <ryansaulsatl@gmail.com>
2014-12-11 20:49:31 -0600

    Unit Tests Added with initial libraries

---
+++ UnitTests/UnitTestExampleOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestExampleOne
+    {
+        [TestMethod]
+        public void TestInitialize()
+        {
+            Examples.ExampleOne example = new Examples.ExampleOne();
+
+            Assert.IsTrue(example.Initialize());
+        }
+    }
+}
